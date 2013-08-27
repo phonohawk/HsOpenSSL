e3a8119d0077e58195c61dc90af83668820ef404
Gregory Collins <greg@gregorycollins.net>
2013-08-27 16:00:57 +0200

    Don't hardcode builddir path (it breaks with "cabal sandbox.").
    Fixes #23, hopefully.

--- cbits/HsOpenSSL.h
+++ cbits/HsOpenSSL.h
@@ -22,12 +22,9 @@
 
 /* A dirty hack to work around for broken versions of Cabal:
  * https://github.com/phonohawk/HsOpenSSL/issues/8
- *
- * The trick is to abuse the fact that -Icbits is always passed to
- * hsc2hs so we can reach the cabal_macros.h from cbits.
  */
 #if !defined(MIN_VERSION_base)
-#  include "../dist/build/autogen/cabal_macros.h"
+#  include "cabal_macros.h"
 #endif
 
 /* OpenSSL ********************************************************************/
